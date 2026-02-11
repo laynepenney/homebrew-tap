7b71b19cf077c0184f0f908d230689ddd70c8060
Layne Penney <dev@layne.pro>
2026-02-11 11:52:29 -0600

    chore: bump gitgrip to v0.13.0

--- Formula/gitgrip.rb
+++ Formula/gitgrip.rb
@@ -2,8 +2,8 @@ class Gitgrip < Formula
   desc "Multi-repo workflow tool for synchronized branches, linked PRs, and atomic merges"
   homepage "https://github.com/laynepenney/gitgrip"
 
-  url "https://github.com/laynepenney/gitgrip/archive/refs/tags/v0.12.3.tar.gz"
-  sha256 "f31e9e37f06a7aafb76738d75eac4dcd5e7c8b59e0a0950beaa78d21e32ecf3c"
+  url "https://github.com/laynepenney/gitgrip/archive/refs/tags/v0.13.0.tar.gz"
+  sha256 "d58a53ddbf3be80c4a0c14cc1f83e7c61cf8b74a96bf131dae3b652da533b753"
 
   license "MIT"
 
